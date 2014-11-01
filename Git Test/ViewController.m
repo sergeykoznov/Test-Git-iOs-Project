d55e48ed8d7766859b7e93d0e924481919f0d34c
Sergey <SergeyKoznov@MacBook-Pro-Sergey.local>
2014-11-01 17:45:53 +0200

    Initial Commit

---
+++ Git Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git Test
+//
+//  Created by Sergey on 11/1/14.
+//  Copyright (c) 2014 com.firmmotion. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
