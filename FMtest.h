52df9c8532ded1877a12c2d74bf94669aa83cc01
Sergey <SergeyKoznov@MacBook-Pro-Sergey.local>
2014-11-01 18:54:31 +0200

    added a test class

---
+++ FMtest.h
@@ -0,0 +1,13 @@
+//
+//  FMtest.h
+//  Git Test
+//
+//  Created by Sergey on 11/1/14.
+//  Copyright (c) 2014 com.firmmotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMtest : NSObject
+
+@end
---
+++ FMtest.m
@@ -0,0 +1,13 @@
+//
+//  FMtest.m
+//  Git Test
+//
+//  Created by Sergey on 11/1/14.
+//  Copyright (c) 2014 com.firmmotion. All rights reserved.
+//
+
+#import "FMtest.h"
+
+@implementation FMtest
+
+@end
